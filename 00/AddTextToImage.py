140fdc28d997450cd4a5a1aa025ed025505817d9
lsabcd <lidingbo_1989@163.com>
2016-07-29 15:49:17 +0800

    00题

--- 00/AddTextToImage.py
+++ 00/AddTextToImage.py
@@ -7,8 +7,8 @@ im = Image.open("resource/test.jpg")
 font = ImageFont.truetype('Arial.ttf', 150)
 draw = ImageDraw.Draw(im)
 (x,y) = im.size
-draw.text((x-150,50),"3",(255,0,0),font=font)
+draw.text((x-150,50),"4",(255,0,0),font=font)
 
-draw = ImageDraw.Draw(im)
+#draw = ImageDraw.Draw(im)
 
 im.save("resource/resoult.jpg")
